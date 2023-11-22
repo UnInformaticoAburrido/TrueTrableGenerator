1ab8d070d26733567cb6fc6d0a4a080d4f692a80
UnInformaticoAburrido <dimitrycompanycifre@gmail.com>
2023-11-22 16:49:47 +0100

    He modificado muchas cosas y aun no lo he terminado pero he podido estender el tamaño del array hasta 26 caracteres. Me falta mejorar la velocidad

--- src/main/java/com/mycompany/meaburro/Objetos/BinaryGroup.java
+++ src/main/java/com/mycompany/meaburro/Objetos/BinaryGroup.java
@@ -3,77 +3,55 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.meaburro.Objetos;
+
 import com.mycompany.meaburro.Objetos.BinaryElement;
+
 /**
  *
  * @author dimit
  */
 public class BinaryGroup {
+
     private int ancho;
     private int altura;
-    private BinaryElement[][] numeros;
-    
+    private int[][] numeros;
+
     public BinaryGroup(int tamano) {
         this.ancho = tamano;
-        this.altura = (int)Math.pow(2, tamano);
-        this.numeros= new BinaryElement[altura][ancho];
+        this.altura = (int) Math.pow(2, tamano);
+        this.numeros = new int[altura][ancho];
         for (int i = 0; i < this.altura; i++) {
             for (int j = 0; j < this.ancho; j++) {
-                this.numeros[i][j] =  new BinaryElement();
+                this.numeros[i][j] = 0;
             }
         }
     }
-    
-    public void tab(){
-        int over=1;
+
+    public void tab() {
+        int over = 1;
         BinaryElement[] numero = new BinaryElement[this.ancho];
         for (int i = 0; i < this.ancho; i++) {
-            numero[i]= new BinaryElement(0);
+            numero[i] = new BinaryElement(0);
         }
         for (int linea = 1; linea < this.altura; linea++) {
-            for (int i = this.ancho; over!=0&& i>0; i--) {
-                over=numero[i-1].mas();
+            for (int i = this.ancho; over != 0 && i > 0; i--) {
+                over = numero[i - 1].mas();
             }
-            over=1;
-            for (int i = 0; i<this.ancho ; i++) {
-                
-                this.numeros[linea][i].setState(numero[i].getState());
+            over = 1;
+            for (int i = 0; i < this.ancho; i++) {
+                this.numeros[linea][i] = numero[i].getState();
             }
         }
         this.show();
     }
-    public void tabE(){
-        int over=1;
-        //BinaryElement[] numero = new BinaryElement[this.ancho];
-        BinaryElement[] numero = new BinaryElement[this.ancho];
-        for (int linea = 1; linea < this.altura; linea++) {
-            for (int i = this.ancho; over!=0&& i>0; i--) {
-                over=numero[i-1].mas();
-            }
-            over=1;
-            for (int i = 0; i<this.ancho ; i++) {
-                
-                this.numeros[linea][i].setState(numero[i].getState());
-            }
-        }
-        this.showE();
-    }
-    private void show(){
+
+    private void show() {
         for (int i = 0; i < this.altura; i++) {
             for (int j = 0; j < this.ancho; j++) {
-                this.numeros[i][j].mostrar();
+                System.out.print(this.numeros[i][j]);
             }
             System.out.println(" ");
         }
-        
-    }
-    private void showE(){
-        for (int i = 0; i < this.altura; i++) {
-            for (int j = 0; j < this.ancho; j++) {
-                this.numeros[i][j].mostrarE();
-            }
-            System.out.println(" ");
-        }
-        
+
     }
 }
